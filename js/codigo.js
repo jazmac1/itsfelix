c536bbfc7ca2a60cdd5a3752896f6c3cc3524649
jazmac1 <135709171+jazmac1@users.noreply.github.com>
2023-06-13 20:02:20 -0300

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,18 @@
+document.getElementById('menu').addEventListener('click', function()
+{
+
+document.getElementById('navega').classList.toggle('mostrar');
+
+
+});
+
+
+function reproducir(){
+document.getElementById('miVideo').play();
+}
+
+
+function pausar(){
+    document.getElementById('miVideo').pause();
+
+}
